3677bab39f6e374e843e5a7e1e491cabcfe74d6e
Rodrigo Asencios <rodrigoasencios99@gmail.com>
2022-09-16 20:46:56 -0500

    implementacion del componente About y su css stylesheet

--- containers/About/About.jsx
+++ containers/About/About.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import {FaAward} from 'react-icons/fa'
+import {MdWork} from 'react-icons/md'
+import {IoIosRocket} from 'react-icons/io'
 
 function About() {
   return (
     <section id='about'>
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src="/assets/me-about.jpg" alt="About Image"></img>  
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <MdWork className='about__icon'/>
+              <h5>Projects</h5>
+              <small>50+</small>
+            </article>
+            <article className='about__card'>
+              <IoIosRocket className='about__icon'/>
+              <h5>Startups</h5>
+              <small>2</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/index.css'
 import '../containers/Header/header.css'
+import '../containers/About/about.css'
 import '../components/Navbar/navbar.css'
 
 function MyApp({ Component, pageProps }) {
