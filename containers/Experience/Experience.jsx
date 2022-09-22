a1e4f864be9b6333732cc04ee8252ddc83e2a083
Rodrigo Asencios <rodrigoasencios99@gmail.com>
2022-09-22 01:00:10 -0500

    implementacion de componente de experiencia y su css stylesheet

--- containers/Experience/Experience.jsx
+++ containers/Experience/Experience.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 function Experience() {
   return (
     <section id='experience'>
-      Experience
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience_datascience">
+          <h3>Data Science</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience_fullstack">
+          <h3>Full Stack Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import '../styles/index.css'
 import '../containers/Header/header.css'
 import '../containers/About/about.css'
+import '../containers/Experience/experience.css'
 import '../components/Navbar/navbar.css'
 
 function MyApp({ Component, pageProps }) {
